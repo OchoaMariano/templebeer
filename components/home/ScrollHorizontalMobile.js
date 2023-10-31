dcd4d3625e725d6c8dda0267dce5f595dc80ecbb
Mariano Ochoa <mariano@digitalfellow.co>
2023-10-30 23:35:14 -0300

    fix scroll lateral

---
+++ components/home/ScrollHorizontalMobile.js
@@ -0,0 +1,35 @@
+'use client'
+
+import React, { useRef, useEffect } from 'react';
+
+function ScrollHorizontal({ children }) {
+  const wrapperRef = useRef(null);
+
+    useEffect(() => {
+        const wrapper = wrapperRef.current;
+
+        const handleScroll = (event) => {
+            console.log("scroll")
+            if (event.deltaY) {
+                // Ajustar esta constante para aumentar o disminuir la "resistencia" al desplazamiento
+                const scrollResistance = 0.7;
+                
+                wrapper.scrollLeft += (event.deltaY + event.deltaX) * scrollResistance;
+                event.preventDefault();
+            }
+        };
+
+        wrapper.addEventListener('wheel', handleScroll);
+        return () => {
+            wrapper.removeEventListener('wheel', handleScroll);
+        };
+    }, []);
+
+  return (
+    <div ref={wrapperRef} className="no-scrollbar" style={{ overflowX: 'auto', overflowY: 'hidden' }}>
+      {children}
+    </div>
+  );
+}
+
+export default ScrollHorizontal;
