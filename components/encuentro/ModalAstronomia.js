241b3b1c6cb92537794fdb3c8fbd0e24e4c64eeb
Mariano Ochoa <mariano@digitalfellow.co>
2023-11-08 16:05:28 -0300

    video astrologia

--- components/encuentro/ModalAstronomia.js
+++ components/encuentro/ModalAstronomia.js
@@ -13,8 +13,19 @@ export default function Modal() {
                     &#8203;
                 </span>
                 <div className="inline-block shadow-xl transform transition-all align-middle ">
-                    <div className="relative w-[94.87vw] h-[118.97vw] md:w-[67.98vh] md:h-[84.98vh]">
-                        <Image src="/encuentros/modal-astronomia.png" fill alt="encuentros-flyer" />
+                <div className="relative w-[94.87vw] h-[118.97vw] md:w-[50.62vh] md:h-[77.47vh]">
+                        <video
+                            src="https://storage.googleapis.com/tb-temporal/astrologi-video.mp4"
+                            className="cursor-pointer"
+                            autoPlay
+                            loop
+                            muted
+                            style={{
+                                objectFit: 'cover',
+                                width: '100%',
+                                height: '100%',
+                            }}
+                        />
                     </div>
                     
                 </div>
