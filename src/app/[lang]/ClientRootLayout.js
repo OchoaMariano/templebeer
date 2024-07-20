aabbddd776a0ff3f161184c867ddf42039ace602
Mariano Ochoa <mariano@digitalfellow.co>
2024-07-19 23:52:10 -0300

    fix /es/form-ipasionales render

--- src/app/[lang]/ClientRootLayout.js
+++ src/app/[lang]/ClientRootLayout.js
@@ -1,7 +1,7 @@
-// app/[lang]/ClientRootLayout.js
 "use client";
 
 import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Head from "next/head";
 import Popup from "../../../components/common/Popup";
 import SelectCountry from "../../../components/common/SelectCountry";
@@ -10,17 +10,20 @@ import Legales from "../../../components/common/Legales";
 export default function ClientRootLayout({ children, params }) {
   const [isLoading, setIsLoading] = useState(true);
   const [showSelectCountry, setShowSelectCountry] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     const selectedCountry = sessionStorage.getItem("selectedCountry");
-    if (!selectedCountry) {
+    const isFormIpasionales = pathname.includes("/form-ipasionales");
+
+    if (!selectedCountry && !isFormIpasionales) {
       setShowSelectCountry(true);
     }
     setIsLoading(false);
-  }, []);
+  }, [pathname]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>; // O un componente de carga más elaborado
   }
 
   return (
